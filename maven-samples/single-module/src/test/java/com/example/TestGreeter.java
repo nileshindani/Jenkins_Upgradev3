5cace659036d2116cf2a49dfd62fb15fd56fb6e6
Nilesh Indani <nilesh.indani@siemens.com>
2020-06-24 23:47:55 +0530

    adding changes to TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "World, Thanks you making me Happy,added new content for maven";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
