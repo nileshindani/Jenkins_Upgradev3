243c5235b4b49ccda6575c5affa68d16583d037b
Nilesh Indani <nilesh.indani@siemens.com>
2020-06-26 12:57:49 +0530

    committing changes to Greeter.java for source code change

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -16,4 +16,8 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
+  
+    public String greet1(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
 }
